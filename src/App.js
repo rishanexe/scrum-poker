4ae7012daa4a8593098f85152a73d18f602f2a68
rishanexe <rishanmascarenhas@gmail.com>
2023-10-29 20:40:44 -0400

    Show current user in table, Discard 0 in average

--- src/App.js
+++ src/App.js
@@ -113,10 +113,15 @@ function App() {
 		}
 		else {
 			var sum = 0
+			var zeroCount = 0
 			poker.forEach((item, index) => {
-				sum = sum + parseInt(item['points'])
+				if (item['points'] == 0) {
+					zeroCount = zeroCount + 1;
+				} else {
+					sum = sum + parseInt(item['points'])
+				}
 			})
-			const average = Math.round(sum / poker.length);
+			const average = Math.round(sum / (poker.length - zeroCount));
 			setAverage(average);
 		}
 	}
@@ -176,7 +181,7 @@ function App() {
 					</Form>
 				</>
 					: <>
-						<h1>{username}</h1>
+						<h1>Hello {username}</h1>
 						<br />
 						<ButtonToolbar aria-label="Toolbar with button groups">
 							<ButtonGroup className="me-2" aria-label="First group">
@@ -209,7 +214,17 @@ function App() {
 								return (
 									<tbody>
 										<tr>
-											<td>{item['name']}</td>
+											<td>
+												{item['name'] == username ?
+													<span style={{ paddingRight: '10px' }}>
+														<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+															<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+															<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+														</svg>
+													</span>
+													: ''}
+												{item['name']}
+											</td>
 											<td>
 												{show == true ? <> {item['points'] == 0 ? '' : item['points']}</> : "Hidden"} {''}
 												{item['points'] != 0 ?
